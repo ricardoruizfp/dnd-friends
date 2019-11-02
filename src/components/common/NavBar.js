7cc66f7970fef6632f8086fd6bdf2abf8bc217c3
ricardoruizfp <rfpimpao@gmail.com>
2019-11-02 17:37:56 +0000

    navbar on small screen

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import './NavBar.css'
 
 function NavBar() {
@@ -16,6 +18,20 @@ function NavBar() {
                 <Link to='#' >Rules FAQ</Link>
                 <Link to='#' >Homebrew</Link>
             </ul>
+            <div className='dropdown'>
+                <FontAwesomeIcon icon={faBars} />
+                <ul className='dropdown-content'>
+                    <Link to='/' >Home</Link>
+                    <Link to='/Characters' >Characters</Link>
+                    <Link to='#' >Current Affairs</Link>
+                    <Link to='#' > Key NPCs</Link>
+                    <Link to='#' >Loot Book</Link>
+                    <Link to='#' >Kill List</Link>
+                    <Link to='#' >Crafting</Link>
+                    <Link to='#' >Rules FAQ</Link>
+                    <Link to='#' >Homebrew</Link>
+                </ul>
+            </div>
             <hr />
         </div>
     )
